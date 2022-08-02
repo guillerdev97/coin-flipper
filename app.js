4f321b09f346f42b644105da6e6747719a8aa252
Guillermo García <guillermieres97@gmail.com>
2022-08-03 01:48:08 +0200

    initial commit and initial implementation of js coin flipper

---
+++ app.js
@@ -0,0 +1,14 @@
+"use stric"
+
+const coinImage = document.querySelector(".coin");
+const doButton = document.getElementById("doButton");
+const resetButton = document.getElementById("resetButton");
+
+const headUrl = "../public/img/head.png";
+const tailUrl = "../public/img/tail.png";
+
+/* doButton.addEventListener("click", );
+function throwingCoin() {
+   
+} */
+
