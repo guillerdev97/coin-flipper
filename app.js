e53468b0b466fbb44e6a2cb808039ea514b206c9
Guillermo García <guillermieres97@gmail.com>
2022-08-08 12:31:35 +0200

    new implementations and development of coin flipper vanilla js

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 "use stric"
 
+// dom elements
 const coinImage = document.querySelector(".coin");
 const doButton = document.getElementById("doButton");
-const resetButton = document.getElementById("resetButton");
 
-const headUrl = "../public/img/head.png";
-const tailUrl = "../public/img/tail.png";
+// variables
+let headCoin = {
+    url: './public/img/head.png',
+    display: true
+}
+let tailCoin = {
+    url: './public/img/tail.png',
+    display: false
+}
 
-/* doButton.addEventListener("click", );
-function throwingCoin() {
-   
-} */
+// event
+doButton.addEventListener("click", runCoin);
 
+// functions
+// run coin function
+function runCoin() {
+    const times = randomNumber(5, 15);
+    let incrementor = 1000;
+    for(let repeat = 0; repeat < times; repeat++) {
+        timeInterval(incrementor);
+        incrementor += 500;
+    }
+}
+
+// random number function
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// change background function
+function changeBackground() {
+    if(headCoin.display === true) {
+        coinImage.style.backgroundImage = `url(${tailCoin.url})`;
+        headCoin.display = false;
+        return;
+    }
+    if(headCoin.display === false) {
+        coinImage.style.backgroundImage = `url(${headCoin.url})`;
+        headCoin.display = true;
+        return;
+    }
+}
+
+// time interval function
+function timeInterval(incrementor) {
+    setTimeout(changeBackground, incrementor);
+}
